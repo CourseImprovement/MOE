71b4aafdea48a82b9126dcfcc0f22fe56d47fe13
Lonestar421 <grantjsheridan@gmail.com>
2015-12-07 14:11:05 -0700

    fixed windows watch issue, dangit windows

--- moe/watch.js
+++ moe/watch.js
@@ -1,5 +1,4 @@
 var colors = require('colors');
-var sys = require('sys')
 var exec = require('child_process').exec;
 
 function help(){
@@ -19,16 +18,48 @@ function Watch(){
 
 function puts(error, stdout, stderr) { if (stderr) console.log(stderr); }
 
+function isValidTime(first, second){
+	return (second - first) > .5;
+}
+
 Watch.prototype.run = function(args){
+	var isWin = /^win/.test(process.platform);
 	var fs = require('fs');
 	console.log('Watching all projects...');
-	fs.watch('./projects', {recursive: true}, function(e, name){
-		if (name.indexOf('build/') > -1) return;
-		var proj = name.split('/')[0];
-		exec("moe index", puts);
-		exec("grunt --base ./projects/" + proj + " --gruntfile ./projects/" + proj + "/GruntFile.js", puts);
-		console.log('Building....');
-	});
+	var lastBuiltPath = '';
+	var lastBuildTime = null;
+
+	if (isWin){
+		fs.watch('.\\projects', {recursive: true}, function(e, name){
+			if (name.indexOf('build\\') > -1) return;
+			if (lastBuiltPath.length == 0) {
+				lastBuiltPath = name;
+				lastBuildTime = new Date();
+			}
+			else{
+				if (lastBuiltPath == name && !isValidTime(lastBuildTime, new Date())){
+					return;
+				}
+			}
+
+			lastBuildTime = new Date();
+
+			var proj = name.split('\\')[0];
+			exec("moe index", puts);
+			exec("grunt --base .\\projects\\" + proj + " --gruntfile .\\projects\\" + proj + "\\GruntFile.js", puts);
+			console.log('Building....');
+		});
+	}
+	else{
+		fs.watch('./projects', {recursive: true}, function(e, name){
+			if (name.indexOf('build/') > -1) return;
+			var proj = name.split('/')[0];
+			exec("moe index", puts);
+			exec("grunt --base ./projects/" + proj + " --gruntfile ./projects/" + proj + "/GruntFile.js", puts);
+			console.log('Building....');
+		});
+	}
+
 }
 
 module.exports = new Watch();
